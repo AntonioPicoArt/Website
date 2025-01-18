fd079f2d3276968f827bc08cb92798481c23fc96
hemangsharma <sharmahemang2000@gmail.com>
2025-01-18 17:47:39 +1100

    new website

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function toggleMenu() {
-    const navMenu = document.getElementById('navMenu');
-    navMenu.classList.toggle('show'); // Toggle the 'show' class to display/hide menu
-  }
-  
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('nav');
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
+
 
 // Smooth scroll with offset to prevent cutting off content
 document.querySelectorAll('nav ul li a').forEach(link => {
@@ -18,4 +22,22 @@ document.querySelectorAll('nav ul li a').forEach(link => {
       });
     });
   });
+  
+  let currentSlideIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+  
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.style.display = i === index ? 'block' : 'none';
+    });
+  }
+  
+  function nextSlide() {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    showSlide(currentSlideIndex);
+  }
+  
+  showSlide(currentSlideIndex);
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
+  
   
